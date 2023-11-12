144d12602d22bf58c1ec4353ec585cc2a47cb32f
Erin Kelley <erin.kelley01@gmail.com>
2023-11-12 08:55:51 -0700

    Complete cleanUp coding challenges

--- cleanUp.js
+++ cleanUp.js
@@ -11,20 +11,19 @@
 function askForName() { console.log("Hello, what is your name?") }
 
 askForName()
-function askForName(){
-  return "Hello, what is your name?")
-}
-askForName();
-
+console.log("Hello, What is your name?");
+// Got rid of the function. Can evoke the desired return with a console.log. 
 // EX 2:
 function addThreeNums(first, second, third) {
 var sum = first + second + third
 return sum;
       }
-
+function addThreeNums(first, second,third){
+  return first + second + third;
+}
 addThreeNums(1, 2, 3);
 addThreeNums(4, 2, 7);
-
+// Got rid of var sum. Can get the desired output by just having the return statement. 
 
 // EX 3:
 func makeFreshPesto(){
@@ -35,7 +34,18 @@ func makeFreshPesto(){
   console.log("Season");    }
 
 makeFreshPesto();
+function makeFreshPesto() {
+  console.log(`
+    Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper
+    Pulse basil and pine nuts
+    Add garlic and cheeses
+    Slowly pour in oil
+    Season
+  `);
+}
 
+makeFreshPesto();
+// Concatenated to join all the strings. 
 
 //  EX 4:
 function average(num1, num2) 
@@ -45,3 +55,9 @@ var sum = num1 + num2;
 
 return avg
   }
+
+ function average(num1, num2){
+  return ((num1 + num2) / 2);
+ } 
+ console.log(average(3,5));
+ // Combined the sum and avg actions to make it one line, instead of having two different actions. 
